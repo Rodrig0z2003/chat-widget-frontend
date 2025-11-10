8e9d368be26c814cef3b206e28882dc0a02fadc0
Rodrig0z2003 <sandro.capira@tecsup.edu.pe>
2025-11-10 16:54:41 -0500

    Arregla CSS global y aplica 'scoped' para PrestaShop

--- src/App.vue
+++ src/App.vue
@@ -321,7 +321,7 @@ watch(messages, async () => {
   </div>
 </template>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
 * {
